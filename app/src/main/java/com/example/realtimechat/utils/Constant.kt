e9413914b762817cd74ecde6e8ef72f225a7c080
manoj <manojcse2017@gmail.com>
2025-12-19 17:20:48 +0530

    Update WS_LIVE_URL with placeholder API key
    Replaced the hardcoded API key in the live WebSocket URL with a "YOUR_API_KEY" placeholder for security.

--- app/src/main/java/com/example/realtimechat/utils/Constant.kt
+++ app/src/main/java/com/example/realtimechat/utils/Constant.kt
@@ -5,5 +5,5 @@ object Constant {
     const val WS_TEST_URL =
         "wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV"
     const val WS_LIVE_URL =
-        "wss://s15585.blr1.piesocket.com/v3/1?api_key=vfIchEceonUQbwGDT4vcaA8vpfnUSZgUl6veubZP"
+        "wss://s15585.blr1.piesocket.com/v3/1?api_key=YOUR_API_KEY"
 }
